6b740b14179dce3f5a9307f5ee92da7d92bfdd47
Daphne Augier <35980232+daphneaugier@users.noreply.github.com>
2020-08-09 20:10:42 +0200

    Moved Employer Job Management out of Employer
    Because of Forbbiden issue on Concordia web site with directories

---
+++ delete-job.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+  include_once "config.inc.php";
+  include_once "functions.inc.php";
+  include_once "job.inc.php";
+  
+$sql = "DELETE FROM jobs WHERE JOB_ID = '".$_GET['id']."';";
+$result = my_query($sql);
+header('location:update-job.php');
